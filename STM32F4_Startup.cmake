31f955263c9a74f8ee7f5a97e462ee0f5fb18378
Philip Schulz <phs@phisch.org>
2020-04-13 22:43:25 +0200

    Put Board-specific Start-up Code and Linker Script into board/ subdirectory. Also, but the Board-specific CMake File there, too.

--- STM32F4_Startup.cmake
+++ STM32F4_Startup.cmake
@@ -1,18 +1,32 @@
-get_property(ARCH_DIR GLOBAL PROPERTY ARCH_DIR)
+###############################################################################
+# Start-up Code
+###############################################################################
+set(TARGET_NAME     startup)
+set(STARTUP_OBJ     ${TARGET_NAME}.o)
 
-get_property(STARTUP_OBJ GLOBAL PROPERTY STARTUP_OBJ)
-get_property(STARTUP_LDSCRIPT GLOBAL PROPERTY STARTUP_LDSCRIPT)
-get_property(STARTUP_TARGET GLOBAL PROPERTY STARTUP_TARGET)
-get_property(CMSIS_ROOT GLOBAL PROPERTY CMSIS_ROOT)
+add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
+    COMMAND ${CMAKE_C_COMPILER} ARGS
+      -c -o ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
+      -nostartfiles -mcpu=cortex-m4 -mthumb
+      ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_STARTUP_CODE}
+    DEPENDS
+     ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_STARTUP_CODE}
+     ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_LDSCRIPT}
+)
+
+add_custom_target(${TARGET_NAME}
+    COMMAND true
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
+)
 
 ###############################################################################
 # Firmware image
 ###############################################################################
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${STARTUP_OBJ} -T ${STARTUP_LDSCRIPT}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${STARTUP_OBJ} -T ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_LDSCRIPT}")
 
 target_link_libraries(${TARGET_ELF}
     cmsis
 )
 add_dependencies(${TARGET_ELF}
-    ${STARTUP_TARGET}
+    startup
 )
--- boards/STM32F4_Discovery/stm32f4-board.cmake
+++ boards/STM32F4_Discovery/stm32f4-board.cmake
@@ -1,6 +1,6 @@
 set(OPENOCD_BOARD_CFG       "stm32f4discovery.cfg")
-set(STM32F4_STARTUP_CODE    "startup_stm32f407xx.s")
-set(STM32F4_LDSCRIPT        "STM32F407VGTx_FLASH.ld")
+set(STM32F4_STARTUP_CODE    "boards/STM32F4_Discovery/startup_stm32f407xx.S")
+set(STM32F4_LDSCRIPT        "boards/STM32F4_Discovery/STM32F407VGTx_FLASH.ld")
 
 add_definitions("-DSTM32F407xx")
 add_definitions("-DHSE_VALUE=((uint32_t)8000000)")
