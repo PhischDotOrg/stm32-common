0d7bd1e8059b29704446e70950b1c9f30e5ce78c
Philip Schulz <phs@phisch.org>
2020-04-27 17:38:22 +0200

    Use CMake features to compile Start-up Assembly code and link with CPU-specific Linker Script.

--- STM32F4_Startup.cmake
+++ STM32F4_Startup.cmake
@@ -2,31 +2,16 @@
 # Start-up Code
 ###############################################################################
 set(TARGET_NAME     startup)
-set(STARTUP_OBJ     ${TARGET_NAME}.o)
 
-add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
-    COMMAND ${CMAKE_C_COMPILER} ARGS
-      -c -o ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
-      -nostartfiles -mcpu=cortex-m4 -mthumb
-      ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_STARTUP_CODE}
-    DEPENDS
-     ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_STARTUP_CODE}
-     ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_LDSCRIPT}
+enable_language(ASM)
+
+add_library(${TARGET_NAME} 
+    ${STM32F4_STARTUP_CODE}    
 )
-
-add_custom_target(${TARGET_NAME}
-    COMMAND true
-    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${STARTUP_OBJ}
+target_compile_options(${TARGET_NAME} PRIVATE
+    -mcpu=cortex-m4
+    -mthumb
 )
-
-###############################################################################
-# Firmware image
-###############################################################################
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${STARTUP_OBJ} -T ${CMAKE_CURRENT_SOURCE_DIR}/${STM32F4_LDSCRIPT}")
-
-target_link_libraries(${TARGET_ELF}
-    cmsis
-)
-add_dependencies(${TARGET_ELF}
-    startup
+target_link_options(${TARGET_NAME} INTERFACE
+    -nostartfiles
 )
