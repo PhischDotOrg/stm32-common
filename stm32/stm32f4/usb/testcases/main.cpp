6d3a60e656966cf0885d59f6a1f93dc1b1f4d89a
Philip Schulz <phs@phisch.org>
2020-11-08 21:48:10 +0100

    Fix USB Unit Tests.

--- stm32/stm32f4/usb/testcases/main.cpp
+++ stm32/stm32f4/usb/testcases/main.cpp
@@ -5,9 +5,36 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <stdio.h>
+
+#if 0
 int
 main(int argc, char **argv) {
     ::testing::InitGoogleMock(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
+#endif
+
+/******************************************************************************/
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined (__cplusplus) */
+/******************************************************************************/
+
+void
+debug_printf(const char * const p_fmt, ...) {
+    va_list va;
+    va_start(va, p_fmt);
+
+    vprintf(p_fmt, va);
+
+    va_end(va);
+}
+
+/******************************************************************************/
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined (__cplusplus) */
+/******************************************************************************/
+
