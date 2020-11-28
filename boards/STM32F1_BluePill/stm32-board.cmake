a131ad6d62302ed6d847348e352396eedd260226
Philip Schulz <phs@phisch.org>
2020-11-28 12:13:47 +0100

    Blue Pill can be flashed with BMP.

--- boards/STM32F1_BluePill/stm32-board.cmake
+++ boards/STM32F1_BluePill/stm32-board.cmake
@@ -3,3 +3,5 @@ set(STM32_STARTUP_CODE      "boards/STM32F1_BluePill/startup_stm32f103xB.S")
 set(STM32_LDSCRIPT          "boards/STM32F1_BluePill/STM32F103xB.ld")
 set(STM32_CPU_FAMILY        "STM32F1")
 set(STM32_CPU_TYPE          "STM32F103xB")
+
+set(STM32_FLASH_INTERFACE   "BlackMagicProbe")
