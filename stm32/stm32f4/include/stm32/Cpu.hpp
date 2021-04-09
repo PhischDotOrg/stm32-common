0acc39357ef7a0dff01118b55d648e1276d30639
Philip Schulz <phs@phisch.org>
2021-04-10 00:23:03 +0200

    Update to other F4xx CPUs.

--- stm32/stm32f4/include/stm32/Cpu.hpp
+++ stm32/stm32f4/include/stm32/Cpu.hpp
@@ -85,6 +85,19 @@ namespace stm32 {
                     template<typename PinT> using Usart2 = ::stm32::UartT<Rcc, USART2_BASE, stm32::BrrPolicyWithOversampling, PinT>;
                     template<typename PinT> using Usart6 = ::stm32::UartT<Rcc, USART6_BASE, stm32::BrrPolicyWithOversampling, PinT>;
                 }; /* struct Uart */
+
+                struct Usb {
+                    using OtgFsCore         = decltype(nullptr);
+                    using Device            = ::stm32::usb::UsbDeviceViaSTM32F4;
+
+                    using CtrlInEndpoint    = ::stm32::usb::CtrlInEndpointViaSTM32F4;
+                    using CtrlOutEndpoint   = ::stm32::usb::CtrlOutEndpointViaSTM32F4;
+
+                    using BulkInEndpoint    = ::stm32::usb::BulkInEndpointViaSTM32F4;
+                    using BulkOutEndpoint   = ::stm32::usb::BulkOutEndpointViaSTM32F4;
+
+                    using IrqInEndpoint     = decltype(nullptr);
+                };                
             }; /* struct Cpu */
         } /* namespace f407 */
 #endif /* defined(STM32F407xx) */
@@ -219,6 +232,19 @@ namespace stm32 {
                     template<typename PinT> using Usart2 = ::stm32::UartT<Rcc, USART2_BASE, stm32::BrrPolicyWithOversampling, PinT>;
                     template<typename PinT> using Usart6 = ::stm32::UartT<Rcc, USART6_BASE, stm32::BrrPolicyWithOversampling, PinT>;
                 }; /* struct Uart */
+
+                struct Usb {
+                    using OtgFsCore         = decltype(nullptr);
+                    using Device            = ::stm32::usb::UsbDeviceViaSTM32F4;
+
+                    using CtrlInEndpoint    = ::stm32::usb::CtrlInEndpointViaSTM32F4;
+                    using CtrlOutEndpoint   = ::stm32::usb::CtrlOutEndpointViaSTM32F4;
+
+                    using BulkInEndpoint    = ::stm32::usb::BulkInEndpointViaSTM32F4;
+                    using BulkOutEndpoint   = ::stm32::usb::BulkOutEndpointViaSTM32F4;
+
+                    using IrqInEndpoint     = decltype(nullptr);
+                };
             }; /* struct Cpu */
         } /* namespace f411 */
 #endif /* defined(STM32F411xE) */
